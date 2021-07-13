83f6e88d9cf085373f56de884e2c8888088fd36f
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2021-07-13 23:36:03 +0300

    Added basic navigation

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,18 +1,29 @@
-import { View,Text,Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet } from "react-native";
 import React from "react";
 
 
 const HomeScreen = (props) => {
 
+    const { navigation } = props
 
+    const detectorSizeHandler = () => {
+        navigation.navigate('DetectSize')
+    }
+    const driCalcHandler = () => {
+        navigation.navigate('DriCalc')
+    }
 
-    return (<View style={styles.container}>  
-        <Text >Home Screen</Text>
-        <Button title='Required Lens Calculator'></Button>
-        <Button title='Detector Size Calculator'></Button>
-        <Button title='Field Of View Calculator'></Button>
-        <Button title='DRI Calculator'></Button>
-        </View>)
+    const fovCalcHandler=()=>{
+        navigation.navigate('FieldOfView')
+    }
+    return (
+    <View style={styles.container}>
+        
+        <Button title='Required Lens Calculator' ></Button>
+        <Button title='Detector Size Calculator' onPress={detectorSizeHandler}></Button>
+        <Button title='Field Of View Calculator' onPress={fovCalcHandler}></Button>
+        <Button title='DRI Calculator' onPress={driCalcHandler}></Button>
+    </View>)
 }
 
 
