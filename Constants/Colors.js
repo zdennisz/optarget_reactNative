016f5da582d81b359919771f0913398a0a924669
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2021-07-21 18:19:29 +0300

    Built skeleton for dri calculator

--- Constants/Colors.js
+++ Constants/Colors.js
@@ -2,5 +2,6 @@ const Colors = {
   Primary: "#000000",
   SecondaryOrangeWhite: "#fef2e9",
   SecondaryOrangeDarker: "#fde5d2",
+  LightGray: "#eff2f4",
 };
 export default Colors;
--- Screens/DRICalculator.js
+++ Screens/DRICalculator.js
@@ -1,15 +1,91 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import {
+  Button,
+  StyleSheet,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
+import React, { useState } from "react";
+import TextInputController from "../Component/TextInputController";
+import { GeneralImages } from "../Component/Images";
+import { focal_length, detector_pitch } from "../Store/Actions/detectorSize";
+import { useSelector, useDispatch } from "react-redux";
+import Colors from "../Constants/Colors";
 const DRICalculator = (props) => {
-  return <Text> DRI Calculator</Text>;
+  const dispatch = useDispatch();
+  const [isValid, setisValid] = useState();
+  let TableComponent;
+  const data = useSelector((state) => state.detectorSize);
+
+  const focalLengthHandler = (val) => {
+    dispatch(focal_length(val));
+  };
+
+  const detectorPitchHandler = (val) => {
+    dispatch(detector_pitch(val));
+  };
+
+  if (isValid) {
+    //Create table layout to fit the two tables that i have
+    //TableComponent = <View></View>;
+  } else {
+    TableComponent = <View></View>;
+  }
+
+  return (
+    <ScrollView>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "android" ? "padding" : "position"}
+      >
+        <View style={styles.container}>
+          <View style={styles.inputContainer}>
+            <TextInputController
+              image={GeneralImages.focalLengthSrc}
+              text="Focal Length (mm): "
+              maxLength={2}
+              handler={focalLengthHandler}
+            />
+            <TextInputController
+              image={GeneralImages.pixelSizeSrc}
+              text="Detector Pitch (mic):  "
+              maxLength={2}
+              handler={detectorPitchHandler}
+            />
+          </View>
+          <View style={styles.btnContainer}>
+            <Button title="Calculate"></Button>
+          </View>
+          <View style={styles.tableContainer}>{TableComponent}</View>
+        </View>
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputContainer: {
+    flex: 1,
+    width: "100%",
+    minHeight: 250,
     justifyContent: "center",
     alignItems: "center",
+  },
+  tableContainer: {
+    flex: 1,
+    width: "100%",
+    minHeight: 50,
+  },
+  btnContainer: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "row-reverse",
+    justifyContent: "flex-start",
+    paddingHorizontal: 15,
   },
 });
 
