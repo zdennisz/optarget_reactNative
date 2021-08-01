1a6ef5e100cb26d5afb4d73566c33d0679a7c4e6
DESKTOP-2C0V7VM\Dennis <Zdennisz@gmail.com>
2021-08-01 18:12:27 +0300

    Added exit on tap outside of popup screen

--- Component/PopupScreen/PopupScreen.js
+++ Component/PopupScreen/PopupScreen.js
@@ -1,46 +1,62 @@
 import React from 'react'
-import { Text, View, StyleSheet, Modal } from 'react-native'
+import { View, StyleSheet, Modal, TouchableWithoutFeedback, Dimensions } from 'react-native'
 import PopupScreenButton from './PopupScreenButton'
 import { GeneralImages, FieldOfViewImages } from '../../Constants/Images'
 import Colors from '../../Constants/Colors'
+
+
 const PopupScreen = (props) => {
-    console.log
+
+
     const { visibility } = props
 
     const pressingHandler = (value) => {
+
         props.handler(value)
     }
 
     return (
 
-        <Modal animationType='slide' transparent={true}
+        <Modal animationType='none' transparent={true}
             visible={visibility} onRequestClose={pressingHandler.bind(this, 'close')} >
-            <View style={styles.buttonContainer} >
-                <PopupScreenButton actionHandler={pressingHandler} action='target' text="Target Size Input" image={GeneralImages.taretSizeSrc} />
-                <PopupScreenButton actionHandler={pressingHandler} action='angle' text="Angle Input" image={FieldOfViewImages.ifovSrc} />
-            </View>
+            <TouchableWithoutFeedback onPress={pressingHandler.bind(this, 'close')}>
+                <View style={styles.modalContainer} onPress={pressingHandler.bind(this, 'close')}>
+                    <View style={styles.container}>
+                        <View style={styles.buttonContainer} >
+                            <View>
+                                <PopupScreenButton actionHandler={pressingHandler} action='target' text="Target Size Input" image={GeneralImages.taretSizeSrc} />
+                                <PopupScreenButton actionHandler={pressingHandler} action='angle' text="Angle Input" image={FieldOfViewImages.ifovSrc} />
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
         </Modal>
+
     )
 }
 
 const styles = StyleSheet.create({
+    modalContainer: {
+        backgroundColor: 'rgba(0,0,0,0.7)',
+        height: "100%",
+
+    },
     container: {
-        flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        width: Dimensions.get("window").width / 2,
+        height: Dimensions.get("window").height / 2,
+        top: Dimensions.get("window").width / 1.9,
+        left: Dimensions.get("window").height / 7
     },
     buttonContainer: {
-        marginTop: '55%',
-        marginHorizontal: '30%',
         flex: 1,
         justifyContent: 'space-around',
         alignItems: 'center',
         backgroundColor: 'white',
-        maxWidth: "50%",
-        minWidth: '20%',
-        maxHeight: '40%',
-        borderRadius: 20,
-
+        padding: 20,
+        borderRadius: 20
     }
 
 })
--- Component/PopupScreen/PopupScreenButton.js
+++ Component/PopupScreen/PopupScreenButton.js
@@ -20,6 +20,7 @@ const PopupScreenButton = (props) => {
 
 const styles = StyleSheet.create({
     container: {
+
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
